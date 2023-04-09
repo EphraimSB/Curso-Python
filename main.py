c58b3dc7da29cf37e56e9390190a5a1dafc289f9
EphraimSB <52842630+EphraimSB@users.noreply.github.com>
2023-04-09 17:29:44 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from persona import Persona
+
+persona1 = Persona ("Efrain", 33)
+persona1.mostrar_datos()
+
+persona1.nombre = "Efrain"
+persona1.mostrar_datos()
+
+print(persona1)
---
+++ persona.py
@@ -0,0 +1,12 @@
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+  
+    
+    def mostrar_datos(self):
+        print("Nombre", self.nombre)
+        print("Edad", self.edad)
+
+    def __str__(self) -> str:
+            return f"Nombre:  {self.nombre} \nEdad: {self.edad}"
